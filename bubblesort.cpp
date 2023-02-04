ce12c8b4e387414471c5d99a878737d4a8e5458e
sanikabaviskar10 <121037077+sanikabaviskar10@users.noreply.github.com>
2023-02-04 15:07:00 +0530

    Create bubblesort.cpp

---
+++ bubblesort.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {10,1,7,6,14,9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    for(int i=1;i<n;i++)
+    {
+        for(int j=0;j<n-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+               swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+}
